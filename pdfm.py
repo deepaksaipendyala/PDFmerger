2a9e114430906a190c2628e64c29f337dba6edd3
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2021-07-14 11:07:52 +0530

    commenting

--- pdfm.py
+++ pdfm.py
@@ -12,12 +12,9 @@ def pdf_combiner(pdf_list):
     pdf=PyPDF2.PdfFileReader(p)
     merger.append(pdf)
   merger.write('merged.pdf')
+  #saved the combined pdf as merged.pdf
 
+#filtering pdf files from the folder and saving them as list
 pdf_list=[a for a in os.listdir(input_folder) if a.endswith(".pdf")]
+#calling the function by sending pdf list saved into it
 pdf_combiner(pdf_list)
-
-# merger=PyPDF2.PdfFileMerger()
-# for pdf in os.listdir(input_folder):
-#   print(pdf)
-#   merger.append(pdf)
-# merger.write('merged.pdf')
